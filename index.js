05358551dfaae23fe35ab3c08d6f9c7946120a63
Abhay Aswal <abhayaswal939@gamil.com>
2024-01-28 22:40:26 +0530

    js done

--- index.js
+++ index.js
@@ -2,14 +2,46 @@ console.log("hlow")
 
 const API_KEY="3717b36f8920461b678b783121fc0e53";
 
-async function showWeather(){
-    // let latitude= 15.3333;
-    // let longitude=74.0833;
+function renderWeatherInfo(data){
+        let newpara=document.createElement('p');
+        newpara.textContent= `${data?.main?.temp.toFixed(2)} °C`;
+    
+        document.body.appendChild(newpara)
+}
 
-    let city="goa";
+async function fetchWeatherDetials(){
 
-    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+    try{
+        let city="kotdwara";
 
-    const data= await response.json();
-    console.log("weather data:->"+ data);
+        const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+        const data= await response.json();
+
+        console.log("weather data:->", data);
+    
+        // let newpara=document.createElement('p');
+        // newpara.textContent= `${data?.main?.temp.toFixed(2)} °C`
+    
+        // document.body.appendChild(newpara)
+        renderWeatherInfo(data);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+async function getCustomWeatherDetails(){
+    try{
+        let latitude=15.6333;
+        let longitude=18.333;
+    
+        let result= await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    
+        let data =await result.json();
+        console.log(result);
+    }
+    catch(err){
+        //handle the error here
+        console.log("Error found",err);
+    }
 }
